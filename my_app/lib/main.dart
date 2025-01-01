1da90f412d3bd87c38212400f47115a04ab1d731
shahriarkabir280 <shahriarkabir280@gmail.com>
2025-01-02 00:49:22 +0600

    Edited main.dart file

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/features/splashScreen.dart';
+import 'package:provider/provider.dart';
 import 'package:my_app/authentications/loginScreen.dart';
+import 'package:my_app/Models/DataModel.dart';
+import 'package:my_app/features/splashScreen.dart';
+
+
 
 
 
@@ -8,22 +12,25 @@ void main(){
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/':(context)=> splashScreen(),
-        '/home': (context) => loginScreen(),// Replace HomeScreen with your main screen
-      },
+    return ChangeNotifierProvider(
+      create: (_) => DataModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) =>splashScreen(),
+          '/home':(context)=>loginScreen(),
+        },
+      ),
     );
   }
-
 }
+
+
 
 
