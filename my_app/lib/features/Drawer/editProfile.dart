247e9b96615ab7ea02e9b01a0fed2de072513b68
shahriarkabir280 <shahriarkabir280@gmail.com>
2025-01-11 18:33:51 +0600

    added backened for edit profile

--- my_app/lib/features/Drawer/editProfile.dart
+++ my_app/lib/features/Drawer/editProfile.dart
@@ -1,6 +1,15 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
+import 'package:my_app/authentications/validator.dart';
+import 'package:my_app/Models/UserState.dart';
+import 'package:my_app/backend_connections/FASTAPI.dart';
 import 'dart:io';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+final FASTAPI fastAPI = FASTAPI();
 
 class EditProfileScreen extends StatefulWidget {
   @override
@@ -11,25 +20,38 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   final _formKey = GlobalKey<FormState>();
   String _name = '';
   String _email = '';
-  String _birthDate = '';
-  File? _image;
+  String _profilePictureUrl = '';
+  File? _image; // For mobile
+  Uint8List? _imageBytes; // For Flutter Web
 
   // Controllers to manage form fields
-  TextEditingController _nameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _birthDateController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
 
-  // List to store dynamic fields and their controllers
-  List<Map<String, dynamic>> _additionalFields = [];
-
-  // Function to pick an image (camera or gallery)
+  @override
+  void initState() {
+    super.initState();
+    final userState = Provider.of<UserState>(context, listen: false);
+    _profilePictureUrl = userState.currentUser?.profilePictureUrl ?? 'No Profile Picture';
+    _nameController.text = userState.currentUser?.name ?? '';
+    _emailController.text = userState.currentUser?.email ?? '';
+  }
   Future<void> _pickImage(ImageSource source) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: source);
-
     if (pickedFile != null) {
       setState(() {
-        _image = File(pickedFile.path);
+        if (kIsWeb) {
+          // For web, use readAsBytes
+          pickedFile.readAsBytes().then((bytes) {
+            setState(() {
+              _imageBytes = bytes;
+            });
+          });
+        } else {
+          // For mobile, use File
+          _image = File(pickedFile.path);
+        }
       });
     }
   }
@@ -39,21 +61,21 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text("Choose Profile Picture"),
+          title: const Text("Choose Profile Picture"),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               ListTile(
-                leading: Icon(Icons.camera_alt),
-                title: Text("Take a Picture"),
+                leading: const Icon(Icons.camera_alt),
+                title: const Text("Take a Picture"),
                 onTap: () {
                   Navigator.pop(context);
                   _pickImage(ImageSource.camera);
                 },
               ),
               ListTile(
-                leading: Icon(Icons.photo_album),
-                title: Text("Choose from Gallery"),
+                leading: const Icon(Icons.photo_album),
+                title: const Text("Choose from Gallery"),
                 onTap: () {
                   Navigator.pop(context);
                   _pickImage(ImageSource.gallery);
@@ -66,94 +88,86 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     );
   }
 
-  Future<void> _selectBirthDate() async {
-    DateTime? pickedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(1900),
-      lastDate: DateTime.now(),
-    );
-    if (pickedDate != null) {
-      setState(() {
-        _birthDate = "${pickedDate.day}/${pickedDate.month}/${pickedDate.year}";
-        _birthDateController.text = _birthDate;
-      });
+  Future<void> _saveProfile(UserState userState) async {
+    final emailError = Validators.validateEmail(_emailController.text);
+    if (emailError != null) {
+      Validators.showSnackBar(context, emailError);
+      return;
+    }
+    if (_formKey.currentState!.validate()) {
+      try {
+        // Handle image upload for web and mobile
+        if (_imageBytes != null || _image != null) {
+          Uint8List imageData;
+          if (_imageBytes != null) {
+            // Use _imageBytes for web
+            imageData = _imageBytes!;
+          } else {
+            // Convert File to Uint8List for mobile
+            imageData = await _image!.readAsBytes();
+          }
+          var result = await fastAPI.updateProfilePicture(context, imageData);
+          setState(() {
+            _profilePictureUrl = result["profile_picture_url"];
+            _imageBytes = null;
+            _image = null;
+          });
+        }
+
+        // Update profile details
+        await fastAPI.editUserProfile(
+          context,
+          _nameController.text,
+          _emailController.text,
+          userState.currentUser?.email ?? '',
+          _profilePictureUrl,
+        );
+        _updateUserInfo(userState);
+        _showSnackbar("Your Profile is Updated Successfully!", Colors.green);
+
+        Future.delayed(const Duration(seconds: 2), () {
+          Navigator.pop(context);
+        });
+      } catch (e) {
+        Validators.showSnackBar(context, "Error: $e");
+      }
     }
   }
 
-  void _showAddMoreOptionsDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text("Add More Options"),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ListTile(
-                leading: Icon(Icons.phone),
-                title: Text("Phone"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _addAdditionalField("Phone");
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Address"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _addAdditionalField("Address");
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.note),
-                title: Text("Others"),
-                onTap: () {
-                  Navigator.pop(context);
-                  _addAdditionalField("Others");
-                },
-              ),
-            ],
-          ),
-        );
-      },
+  void _updateUserInfo(UserState userState) {
+    final updatedUser = User(
+      name: _nameController.text,
+      email: _emailController.text,
+      groups: userState.currentUser?.groups ?? [],
+      loginStatus: userState.currentUser?.loginStatus ?? false,
+      createdAt: userState.currentUser?.createdAt ?? '',
+      profilePictureUrl: _profilePictureUrl,
     );
+    userState.updateUser(updatedUser);
   }
 
-  void _addAdditionalField(String fieldName) {
-    setState(() {
-      _additionalFields.add({
-        "field": fieldName,
-        "controller": TextEditingController(),
-      });
-    });
-  }
-
-  void _deleteAdditionalField(int index) {
-    setState(() {
-      _additionalFields[index]["controller"].dispose();
-      _additionalFields.removeAt(index);
-    });
+  void _showSnackbar(String message, Color color) {
+    final snackBar = SnackBar(
+      content: Text(message),
+      backgroundColor: color,
+      duration: const Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 
   @override
   void dispose() {
-    // Dispose of all controllers to avoid memory leaks
     _nameController.dispose();
     _emailController.dispose();
-    _birthDateController.dispose();
-    for (var field in _additionalFields) {
-      field["controller"].dispose();
-    }
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    final userState = Provider.of<UserState>(context, listen: true);
     return Scaffold(
       appBar: AppBar(
-        title: Text('Edit My Profile'),
+        title: const Text('Edit My Profile'),
         backgroundColor: Colors.teal,
       ),
       body: Padding(
@@ -167,9 +181,13 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   children: [
                     CircleAvatar(
                       radius: 50,
-                      backgroundImage: _image == null
-                          ? AssetImage('assets/user_image.png') // Placeholder
-                          : FileImage(_image!) as ImageProvider,
+                      backgroundImage: _imageBytes != null
+                          ? MemoryImage(_imageBytes!)
+                          : _image != null
+                          ? FileImage(_image!)
+                          : (_profilePictureUrl.isNotEmpty
+                          ? NetworkImage(_profilePictureUrl)
+                          : const AssetImage('assets/default_profile_picture.png')) as ImageProvider,
                       backgroundColor: Colors.grey[300],
                     ),
                     Positioned(
@@ -177,7 +195,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                       right: 0,
                       child: GestureDetector(
                         onTap: _showImageSourceDialog,
-                        child: CircleAvatar(
+                        child: const CircleAvatar(
                           radius: 18,
                           backgroundColor: Colors.blue,
                           child: Icon(Icons.camera_alt, color: Colors.white, size: 18),
@@ -187,83 +205,62 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
                   ],
                 ),
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 30),
               TextFormField(
                 controller: _nameController,
                 decoration: InputDecoration(
-                  hintText: 'Enter your name',
                   labelText: 'Name',
-                  prefixIcon: Icon(Icons.person),
+                  labelStyle: TextStyle(color: Colors.teal),
+                  prefixIcon: Icon(Icons.person, color: Colors.teal),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.teal),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.teal, width: 2),
+                  ),
                 ),
-                onChanged: (value) => setState(() => _name = value),
+                validator: (value) =>
+                value == null || value.isEmpty ? 'Please enter your name' : null,
               ),
               SizedBox(height: 20),
               TextFormField(
                 controller: _emailController,
                 decoration: InputDecoration(
-                  hintText: 'Enter your email',
                   labelText: 'Email',
-                  prefixIcon: Icon(Icons.email),
+                  labelStyle: TextStyle(color: Colors.teal),
+                  prefixIcon: Icon(Icons.email, color: Colors.teal),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.teal),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide(color: Colors.teal, width: 2),
+                  ),
                 ),
                 keyboardType: TextInputType.emailAddress,
-                onChanged: (value) => setState(() => _email = value),
+                validator: (value) =>
+                value == null || value.isEmpty ? 'Please enter your email' : null,
               ),
-              SizedBox(height: 20),
-              TextField(
-                controller: _birthDateController,
-                decoration: InputDecoration(
-                  hintText: 'Pick your birth date',
-                  labelText: 'Birth Date',
-                  prefixIcon: Icon(Icons.calendar_today),
+              SizedBox(height: 40),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () => _saveProfile(userState),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.teal,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: Text(
+                    'Save Changes',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
                 ),
-                readOnly: true,
-                onTap: _selectBirthDate,
-              ),
-              SizedBox(height: 20),
-              GestureDetector(
-                onTap: _showAddMoreOptionsDialog,
-                child: Text('Add More Options', style: TextStyle(color: Colors.blue, fontSize: 16)),
-              ),
-              SizedBox(height: 20),
-              for (int i = 0; i < _additionalFields.length; i++) ...[
-                Row(
-                  children: [
-                    Icon(
-                      _additionalFields[i]["field"] == "Phone"
-                          ? Icons.phone
-                          : _additionalFields[i]["field"] == "Address"
-                          ? Icons.home
-                          : Icons.note,
-                    ),
-                    SizedBox(width: 8),
-                    Expanded(
-                      child: TextField(
-                        controller: _additionalFields[i]["controller"],
-                        decoration: InputDecoration(
-                          hintText: 'Enter your ${_additionalFields[i]["field"]}',
-                        ),
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.close, color: Colors.red),
-                      onPressed: () => _deleteAdditionalField(i),
-                    ),
-                  ],
-                ),
-                Divider(),
-              ],
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    print("Name: $_name");
-                    print("Email: $_email");
-                    print("Birth Date: $_birthDate");
-                    for (var field in _additionalFields) {
-                      print("${field['field']}: ${field['controller'].text}");
-                    }
-                  }
-                },
-                child: Text('Save Profile'),
               ),
             ],
           ),
