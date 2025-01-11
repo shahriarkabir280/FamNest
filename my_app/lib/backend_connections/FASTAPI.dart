28edc689b7573542305005357c2a906655cc5a88
shahriarkabir280 <shahriarkabir280@gmail.com>
2025-01-11 18:08:20 +0600

    updated

--- my_app/lib/backend_connections/FASTAPI.dart
+++ my_app/lib/backend_connections/FASTAPI.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/backend_connections/api services/authentications/authentication.dart';
 
+import 'dart:typed_data';
+
+
 class FASTAPI {
+
   // For user registration
   Future<Map<String, dynamic>> registerUser(
-      BuildContext context, String name, String email, String password) async {
-    return await BackendService.registerUser(name, email, password);
+      BuildContext context, String name, String email, String password, String profile_picture_url) async {
+    return await BackendService.registerUser(name, email, password, profile_picture_url);
   }
-
   // For user login
   Future<Map<String, dynamic>> loginUser(
       BuildContext context, String email, String password) async {
@@ -25,6 +28,23 @@ class FASTAPI {
       String groupName, String groupCode) async {
     return await BackendService.createGroup(email, groupName, groupCode);
   }
+  // Join Group
+  Future<Map<String, dynamic>> joinGroup(
+      BuildContext context, String email, String groupCode) async {
+    return await BackendService.joinGroup(email, groupCode);
+  }
+
+  // Leave Group
+  Future<Map<String, dynamic>> leaveGroup(
+      BuildContext context, String email, String groupCode) async {
+    return await BackendService.leaveGroup(email, groupCode);
+  }
+
+  // Update Current Group
+  Future<Map<String, dynamic>> updateCurrentGroup(
+      BuildContext context, String email, String groupCode) async {
+    return await BackendService.updateCurrentGroup(email, groupCode);
+  }
 
   // For finding a group
   Future<Map<String, dynamic>> findGroup(
@@ -32,26 +52,62 @@ class FASTAPI {
     return await BackendService.findGroup(groupCode);
   }
 
+  // Get Group Members
+  Future<Map<String, dynamic>> getGroupMembers(
+      BuildContext context, String groupCode) async {
+    return await BackendService.getGroupMembers(groupCode);
+  }
+
+  Future<Map<String, dynamic>> removeGroupMember(
+      BuildContext context, String groupCode, String email) async {
+    return await BackendService.removeGroupMember(groupCode, email);
+  }
+
+
   // For fetching user data
   Future<Map<String, dynamic>> getUserData(BuildContext context, String email) async {
     return await BackendService.getUserData(email);
   }
 
 
-  // For forgot password functionality
-  Future<Map<String, dynamic>> forgotPassword(
+// Forgot Password: Send Reset Code
+  Future<Map<String, dynamic>> sendResetCode(
       BuildContext context, String email) async {
     return await BackendService.forgotPassword(email);
   }
 
-  // For resetting the password
-  Future<Map<String, dynamic>> resetPassword(BuildContext context, String email,
-      String newPassword) async {
-    return await BackendService.resetPassword(email, newPassword);
+
+  // Reset Password
+  Future<Map<String, dynamic>> resetPassword(
+      BuildContext context,
+      String email,
+      String resetCode,
+      String newPassword,
+      ) async {
+    return await BackendService.resetPassword(email, resetCode, newPassword);
   }
 
   // For fetching all users
   Future<List<dynamic>> getAllUsers(BuildContext context) async {
     return await BackendService.getAllUsers();
   }
+
+  // For editing profile
+  Future<Map<String, dynamic>> editUserProfile(
+      BuildContext context, String new_name, String new_email , String old_email, String profile_picture_url) async {
+    return await BackendService.editUserProfile(new_name, new_email, old_email,profile_picture_url);
+  }
+  // For Uploading Profile Picture
+  Future<Map<String, dynamic>> updateProfilePicture(
+      BuildContext context, Uint8List imageBytes) async {
+    return await BackendService.uploadProfilePicture(imageBytes);
+  }
+
+  // Change Password
+  Future<Map<String, dynamic>> ChangeUserPassword(
+      BuildContext context, String email, String new_password) async {
+    return await BackendService.ChangeUserPassword(email, new_password);
+  }
+
+
 }
