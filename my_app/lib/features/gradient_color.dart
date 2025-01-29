39cd2d9e2fe73bf63c65d9724ee5d76bac4492bf
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-01-29 11:19:10 +0600

    Add files via upload

---
+++ my_app/lib/features/gradient_color.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppGradients {
+  static LinearGradient blueToPurple = LinearGradient(
+    colors: [Colors.lightBlueAccent.withOpacity(0.8), Colors.purple.withOpacity(.6)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+
+  static LinearGradient greenToBlue = LinearGradient(
+    colors: [Colors.green, Colors.blue],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  static LinearGradient orangeToRed = LinearGradient(
+    colors: [Colors.orange, Colors.red],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight,
+  );
+}
+class DynamicGradient {
+  static LinearGradient createGradient(List<Color> colors, Alignment begin, Alignment end) {
+    return LinearGradient(
+      colors: colors,
+      begin: begin,
+      end: end,
+    );
+  }
+}
+
