15d43da1d52df5ac8f01c7182fb140c5399cf3c8
Ovijit Chandra Balo <143479365+Ovijit-Balo@users.noreply.github.com>
2025-01-29 12:27:21 +0600

    Update category_description_list.dart

--- my_app/lib/features/Expense Tracking/widgets/category_description_list.dart
+++ my_app/lib/features/Expense Tracking/widgets/category_description_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:testapp/Models/DataModel.dart';
+import 'package:my_app/Models/DataModel.dart';
 
 class CategoryDescriptionList extends StatelessWidget {
   final DataModel dataModel;
