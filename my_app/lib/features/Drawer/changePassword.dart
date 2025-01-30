95bc140b16906301fea3b515606cc895c3eca1f1
Faiaz Mahmud Ifti <146048013+FaiazMahmud@users.noreply.github.com>
2025-01-30 23:26:17 +0600

    Fixed changePassword.dart

--- my_app/lib/features/Drawer/changePassword.dart
+++ my_app/lib/features/Drawer/changePassword.dart
@@ -1,3 +1,217 @@
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+// // import 'package:my_app/authentications/validator.dart';
+// import 'package:my_app/Models/UserState.dart';
+// import 'package:my_app/backend_connections/FASTAPI.dart';
+
+// final FASTAPI fastAPI = FASTAPI();
+
+// class ChangePasswordScreen extends StatefulWidget {
+//   @override
+//   _ChangePasswordScreenState createState() => _ChangePasswordScreenState();
+// }
+
+// class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
+//   final _formKey = GlobalKey<FormState>();
+//   final TextEditingController _currentPasswordController = TextEditingController();
+//   final TextEditingController _newPasswordController = TextEditingController();
+//   final TextEditingController _confirmPasswordController = TextEditingController();
+
+//   bool _isCurrentPasswordVisible = false;
+//   bool _isNewPasswordVisible = false;
+//   bool _isConfirmPasswordVisible = false;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final userState = Provider.of<UserState>(context, listen: true);
+//     final email = userState.currentUser?.email ?? 'No email available';
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.teal,
+//         title: Text(
+//           'Change Password',
+//           style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),
+//         ),
+//         centerTitle: true,
+//         iconTheme: IconThemeData(color: Colors.white),
+//         // This centers the title text
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Form(
+//           key: _formKey,
+//           child: ListView(
+//             children: [
+//               // Current Password Field
+//               TextFormField(
+//                 controller: _currentPasswordController,
+//                 obscureText: !_isCurrentPasswordVisible,
+//                 decoration: InputDecoration(
+//                   labelText: 'Current Password',
+//                   prefixIcon: Icon(Icons.lock_outlined, color: Colors.teal),
+//                   suffixIcon: IconButton(
+//                     icon: Icon(
+//                       _isCurrentPasswordVisible ? Icons.visibility : Icons.visibility_off,
+//                       color: Colors.teal,
+//                     ),
+//                     onPressed: () {
+//                       setState(() {
+//                         _isCurrentPasswordVisible = !_isCurrentPasswordVisible;
+//                       });
+//                     },
+//                   ),
+//                   focusedBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.teal), // Teal color when focused
+//                   ),
+//                   enabledBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.grey), // Grey color when not focused
+//                   ),
+//                 ),
+//                 validator: (value) {
+//                   if (value == null || value.isEmpty) {
+//                     return 'Please enter your current password';
+//                   }
+//                   return null;
+//                 },
+//               ),
+//               SizedBox(height: 20),
+
+//               // New Password Field
+//               TextFormField(
+//                 controller: _newPasswordController,
+//                 obscureText: !_isNewPasswordVisible,
+//                 decoration: InputDecoration(
+//                   labelText: 'New Password',
+//                   prefixIcon: Icon(Icons.lock_outlined, color: Colors.teal),
+//                   suffixIcon: IconButton(
+//                     icon: Icon(
+//                       _isNewPasswordVisible ? Icons.visibility : Icons.visibility_off,
+//                       color: Colors.teal,
+//                     ),
+//                     onPressed: () {
+//                       setState(() {
+//                         _isNewPasswordVisible = !_isNewPasswordVisible;
+//                       });
+//                     },
+//                   ),
+//                   // focusedBorder: UnderlineInputBorder(
+//                   //   borderSide: BorderSide(color: Colors.teal),
+//                   // ),
+//                   // enabledBorder: UnderlineInputBorder(
+//                   //   borderSide: BorderSide(color: Colors.grey),
+//                   // ),
+//                 ),
+//                 // validator: (value) {
+//                 //   if (value == null || value.isEmpty) {
+//                 //     return 'Please enter a new password';
+//                 //   }
+//                 //   if (value.length < 6) {
+//                 //     return 'Password must be at least 6 characters long';
+//                 //   }
+//                 //   return null;
+//                 // },
+//               ),
+//               SizedBox(height: 20),
+
+//               // Confirm New Password Field
+//               TextFormField(
+//                 controller: _confirmPasswordController,
+//                 obscureText: !_isConfirmPasswordVisible,
+//                 decoration: InputDecoration(
+//                   labelText: 'Confirm New Password',
+//                   prefixIcon: Icon(Icons.lock_outlined, color: Colors.teal),
+//                   suffixIcon: IconButton(
+//                     icon: Icon(
+//                       _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+//                       color: Colors.teal,
+//                     ),
+//                     onPressed: () {
+//                       setState(() {
+//                         _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+//                       });
+//                     },
+//                   ),
+//                   focusedBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.teal),
+//                   ),
+//                   enabledBorder: UnderlineInputBorder(
+//                     borderSide: BorderSide(color: Colors.grey),
+//                   ),
+//                 ),
+//                 // validator: (value) {
+//                 //   if (value == null || value.isEmpty) {
+//                 //     return 'Please confirm your new password';
+//                 //   }
+//                 //   if (value != _newPasswordController.text) {
+//                 //     return 'Passwords do not match';
+//                 //   }
+//                 //   return null;
+//                 // },
+//               ),
+//               SizedBox(height: 20),
+
+//               // Change Password Button with teal stroke and white fill
+//               ElevatedButton(
+//                 onPressed: () async {
+//                   final newPasswordError = Validators.validatePassword(_newPasswordController.text);
+//                   final confirmNewPasswordError = Validators.validateConfirmPassword(
+//                     _newPasswordController.text,
+//                     _confirmPasswordController.text,
+//                   );
+
+//                   if (newPasswordError != null) {
+//                     Validators.showSnackBar(context, newPasswordError);
+//                     return;
+//                   }
+
+//                   if (confirmNewPasswordError != null) {
+//                     Validators.showSnackBar(context, confirmNewPasswordError);
+//                     return;
+//                   }
+//                   try {
+//                     final loginResponse = await fastAPI.loginUser(context, email, _currentPasswordController.text);
+//                     if (loginResponse.containsKey("access_token")) {
+//                       await fastAPI.ChangeUserPassword(context, email, _newPasswordController.text);
+//                       Validators.showSnackBar(
+//                         context,
+//                         "Password Changed Successfully",
+//                         backgroundColor: Colors.lightGreen,
+//                       );
+//                       Future.delayed(const Duration(seconds: 2), () {
+//                         Navigator.pop(context);
+//                       });
+//                     } else {
+//                       ScaffoldMessenger.of(context).showSnackBar(
+//                         SnackBar(content: Text("Failed to Change Password. Please try again.")),
+//                       );
+//                     }
+//                   } catch (e) {
+//                     ScaffoldMessenger.of(context).showSnackBar(
+//                       SnackBar(content: Text("Current Password is INCORRECT!!"),backgroundColor: Colors.redAccent,),
+//                     );
+//                   }
+//                 },
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.teal[700],
+//                   padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(30),
+//                   ),
+//                 ),
+//                 child: const Text(
+//                   "Change Password",
+//                   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:my_app/authentications/validator.dart';
@@ -102,15 +316,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                     borderSide: BorderSide(color: Colors.grey),
                   ),
                 ),
-                // validator: (value) {
-                //   if (value == null || value.isEmpty) {
-                //     return 'Please enter a new password';
-                //   }
-                //   if (value.length < 6) {
-                //     return 'Password must be at least 6 characters long';
-                //   }
-                //   return null;
-                // },
               ),
               SizedBox(height: 20),
 
@@ -139,15 +344,6 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
                     borderSide: BorderSide(color: Colors.grey),
                   ),
                 ),
-                // validator: (value) {
-                //   if (value == null || value.isEmpty) {
-                //     return 'Please confirm your new password';
-                //   }
-                //   if (value != _newPasswordController.text) {
-                //     return 'Passwords do not match';
-                //   }
-                //   return null;
-                // },
               ),
               SizedBox(height: 20),
 
